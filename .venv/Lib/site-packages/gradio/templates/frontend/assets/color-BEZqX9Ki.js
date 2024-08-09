ef45922d62323aa6c7bb10e7d30e2701124953dd
AhmadMkhlalati7 <AhmadMkhlalati7@gmail.com>
2024-08-09 13:37:50 +0400

    add gradio

---
+++ .venv/Lib/site-packages/gradio/templates/frontend/assets/color-BEZqX9Ki.js
@@ -0,0 +1,2 @@
+import{o}from"./Index-DyMt_EUv.js";const t=r=>o[r%o.length];export{t as g};
+//# sourceMappingURL=color-BEZqX9Ki.js.map
---
+++ .venv/Lib/site-packages/gradio/templates/frontend/assets/index-yxdXv0Pw.js
@@ -0,0 +1,2 @@
+import{L as e,S as m,S as p}from"./Index-DyMt_EUv.js";import{T as i}from"./Blocks-D7wgXFF4.js";import"./index-NyBnvGBo.js";import"./svelte/svelte.js";import"./Button-BuDwGXBe.js";export{e as Loader,m as StatusTracker,i as Toast,p as default};
+//# sourceMappingURL=index-yxdXv0Pw.js.map
